3bb4724a9b989a808bfc56c635cdcefe9fa6d6ec
Corentin Gautier <36226516+Adorr29@users.noreply.github.com>
2020-12-12 18:18:16 +0100

    Use Idle animations un Player script

--- Petit Monstre/Assets/Scripts/Player.cs
+++ Petit Monstre/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public float speed = 1;
 
     Vector2 direction = Vector2.zero;
+    Vector2 prevDirection = Vector2.zero;
 
     Animator animator;
 
@@ -27,6 +28,7 @@ public class Player : MonoBehaviour
     {
         UpdateWalkAnimation();
         transform.Translate(direction.normalized * speed * Time.fixedDeltaTime);
+        prevDirection = direction;
     }
 
     void UpdateWalkAnimation()
@@ -39,5 +41,16 @@ public class Player : MonoBehaviour
             animator.Play("WalkUp");
         else if (direction.y < 0)
             animator.Play("WalkDown");
+        else
+        {
+            if (prevDirection.x > 0)
+                animator.Play("IdleRight");
+            else if (prevDirection.x < 0)
+                animator.Play("IdleLeft");
+            else if (prevDirection.y > 0)
+                animator.Play("IdleUp");
+            else if (prevDirection.y < 0)
+                animator.Play("IdleDown");
+        }
     }
 }
